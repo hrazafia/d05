f4e881be0562efbdaaf433be6dabcd80d5de1a37
Herinirina Livasoa Razafiandriatsima <hrazafia@c1r3s6.42antananarivo.mg>
2025-05-02 17:19:45 +0300

    update project

--- ex01/Form.hpp
+++ ex01/Form.hpp
@@ -6,8 +6,26 @@
 /*   By: hrazafia <hrazafia@student.42antanana      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/02 13:58:09 by hrazafia          #+#    #+#             */
-/*   Updated: 2025/05/02 14:52:40 by hrazafia         ###   ########.fr       */
+/*   Updated: 2025/05/02 17:11:36 by hrazafia         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#ifndef FORM_HPP
+# define FORM_HPP
 
+# include <iostream>
+# include <string>
+# include <stdexcept>
+
+class Form
+{
+	private:
+		const std::string	_name;
+		bool				_signed;
+		const int			_signGrade;
+		const int			_executeGrade;
+
+	public:
+};
+
+#endif
