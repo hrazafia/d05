ecd640746f4749f55125483a221b1b35e29fa8b4
Herinirina Livasoa Razafiandriatsima <hrazafia@c1r13s7.42antananarivo.mg>
2025-05-27 12:09:22 +0300

    update project

--- ex01/Form.cpp
+++ ex01/Form.cpp
@@ -6,7 +6,7 @@
 /*   By: hrazafia <hrazafia@student.42antanana      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/27 06:18:59 by hrazafia          #+#    #+#             */
-/*   Updated: 2025/05/27 11:32:31 by hrazafia         ###   ########.fr       */
+/*   Updated: 2025/05/27 12:02:48 by hrazafia         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,4 +30,41 @@ Form::Form(const Form& other): name(other.name), isSigned(other.isSigned),
 Form::~Form()
 {
 
+}
+
+const str::string&	Form::getName() const throw()
+{
+	return (this->name);
+}
+
+bool	Form::getIsSigned() const throw()
+{
+	return (this->isSigned);
+}
+
+const int	getSignGrade() const throw()
+{
+	return (this->signGrade);
+}
+
+const int	getExecGrade() const throw()
+{
+	return (this->execGrade);
+}
+
+const char*	Form::GradeTooHighException::what() const throw()
+{
+	return ("Form grade is too hight!");
+}
+
+const char* Form::GradeTooLowException::what() const throw()
+{
+	return ("Form grade is too low!");
+}
+
+std::ostream&	operator<<(std::ostream& out, const Form& form)
+{
+	out << form.getName() << ", sign grade required " << form.getSignGrade()
+		<< ", execute grade required " << form.getExecGrade()
+		<< ", is " << form.getIsSigned() ? "signed." : "not signed.";
 }
